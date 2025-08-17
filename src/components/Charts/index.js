59746233d43e24bb4d3a3a847c13d7c791685546
masud001 <madhnagar@gmail.com>
2025-08-18 00:06:34 +0600

    Remove unused components and assets: Deleted App.css, App.jsx, main.jsx, various SVG icons, images, and several chart components to streamline the project and improve maintainability.

--- src/components/Charts/index.js
+++
@@ -1,3 +0,0 @@
-export { default as MonthlyUsageChart } from './MonthlyUsageChart';
-export { default as HazardDistributionChart } from './HazardDistributionChart';
-export { default as ComplianceTrackingChart } from './ComplianceTrackingChart';
--- src/components/UI/index.js
+++
@@ -1,4 +0,0 @@
-export { default as Button } from './Button';
-export { default as LoadingSpinner } from './LoadingSpinner';
-export { default as SearchInput } from './SearchInput';
-export { default as ErrorMessage } from './ErrorMessage';
